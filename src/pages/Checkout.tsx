da79535783e957393a0fccddf37a56b98f94a506
Mateus Garcia <mateussk83@gmail.com>
2022-09-14 12:48:01 -0300

    feat: insert forma de pagamento in checkout

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,4 +1,4 @@
-import { CurrencyDollar, MapPinLine, Money } from "phosphor-react";
+import { Bank, CreditCard, CurrencyDollar, MapPinLine, Money } from "phosphor-react";
 
 export function Checkout() {
  return (
@@ -16,24 +16,29 @@ export function Checkout() {
         <label htmlFor="">Informe o endereço onde deseja receber</label>
        </div>
       </div>
-      <input type="text" className="bg-gray-300 text-gray-600 border-gray-400 p-3 mt-8 w-[200px] rounded-md placeholder:text-gray-600" placeholder="CEP" />
-      <input type="text" className="bg-gray-300 text-gray-600 border-gray-400 p-3 w-[560px] mt-4 rounded-md placeholder:text-gray-600" placeholder="Rua" />
+      <input type="text" className="bg-gray-300 text-gray-600 border border-gray-400 p-3 mt-8 w-[200px] rounded-md placeholder:text-gray-600" placeholder="CEP" />
+      <input type="text" className="bg-gray-300 text-gray-600 border  border-gray-400 p-3 w-[560px] mt-4 rounded-md placeholder:text-gray-600" placeholder="Rua" />
       <div className="flex mt-4 gap-3">
        <input type="text" className="bg-gray-300 text-gray-600 border border-gray-400 p-3 rounded-md placeholder:text-gray-600" placeholder="Número" />
-       <input type="text" className="bg-gray-300 text-gray-600 border-gray-400 p-3 rounded-md placeholder:text-gray-600" placeholder="Complemento" />
+       <input type="text" className="bg-gray-300 text-gray-600 border  border-gray-400 p-3 rounded-md placeholder:text-gray-600" placeholder="Complemento" />
       </div>
       <div className="flex mt-4 gap-3">
-       <input type="text" className="bg-gray-300 text-gray-800 border-gray-400 p-3 rounded-md placeholder:text-gray-600" placeholder="Bairro" />
-       <input type="text" className="bg-gray-300 text-gray-600 border-gray-400 p-3 rounded-md placeholder:text-gray-600" placeholder="Cidade" />
-       <input type="text" className="bg-gray-300 text-gray-600 border-gray-400 p-3 rounded-md placeholder:text-gray-600" placeholder="UF" />
+       <input type="text" className="bg-gray-300 text-gray-800 border  border-gray-400 p-3 rounded-md placeholder:text-gray-600" placeholder="Bairro" />
+       <input type="text" className="bg-gray-300 text-gray-600 border  border-gray-400 p-3 rounded-md placeholder:text-gray-600" placeholder="Cidade" />
+       <input type="text" className="bg-gray-300 text-gray-600 border  border-gray-400 p-3 rounded-md placeholder:text-gray-600" placeholder="UF" />
       </div>
      </form>
-     <div className="flex ">
-      <div className="flex items-start justify-start gap-2">
+     <div className="flex bg-gray-200 mt-3 p-10">
+      <div className="flex gap-2 ">
        <CurrencyDollar size={25} color="#5f3dc4" weight="bold" />
-       <div className="flex-col flex">
+       <div className="flex-col flex ">
         <strong>Pagamento</strong>
         <span>O pagamento é feito na entrega. Escolha a forma que deseja pagar</span>
+        <div className="flex items-center justify-center gap-3 pt-8">
+         <a href="" className="font-normal flex items-center justify-center bg-gray-400 text-xs p-4 w-[178px] h-[51px] border-none rounded-md gap-3"><CreditCard size={16} color="#5f3dc4"/>CARTÂO DE CRÉDITO</a>
+         <a href="" className="font-normal flex items-center justify-center bg-gray-400 text-xs p-4 w-[178px] h-[51px] border-none rounded-md gap-3"><Bank size={16} color="#5f3dc4"/>CARTÂO DE DÉBITO</a>
+         <a href="" className="font-normal flex items-center justify-start text-start bg-gray-400 text-xs p-4 w-[178px] h-[51px] border-none rounded-md gap-3"><Money size={16} color="#5f3dc4"/>DINHEIRO</a>
+        </div>
        </div>
       </div>
      </div>
